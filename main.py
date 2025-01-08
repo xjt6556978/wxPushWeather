6d5649585817674fbba0e4f475eb8b94f565fba8
xjt6556978 <93774442+xjt6556978@users.noreply.github.com>
2025-01-09 00:01:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ def get_birthday():
     if next < datetime.now():
       #next = next.replace(year=next.year + 1)
       next = next.replace(year=next.year + 1)
-    return (next - today).days
+    return (next - today).days+1
 
 
 # 彩虹屁接口
